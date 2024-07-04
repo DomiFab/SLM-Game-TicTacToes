b8ae56fe70b146fa625fd816dde16ee7c3c2a862
Neliax <xialin.ye1996@hotmail.com>
2024-07-05 00:05:30 +0200

    Added Game Result Functionality

--- src/main/java/ticTacToe/TicTacToe.java
+++ src/main/java/ticTacToe/TicTacToe.java
@@ -21,7 +21,7 @@ public class TicTacToe {
 
     public void start() {
         Scanner scanner = new Scanner(System.in);
-        while (/*!hasWinner() &&*/ !board.isFull()) {
+        while (!hasWinner() && !board.isFull()) {
             board.print();
             System.out.println("Current Player: " + currentPlayer.getMarker());
             System.out.print("row (0-2): ");
@@ -30,7 +30,7 @@ public class TicTacToe {
             int col = scanner.nextInt();
             if (board.isCellEmpty(row, col)) {
                 board.place(row, col, currentPlayer.getMarker());
-                if (/*hasWinner()*/true) {
+                if (hasWinner()) {
                     board.print();
                     System.out.println("Player " + currentPlayer.getMarker() + " wins!");
                     return;
@@ -48,5 +48,24 @@ public class TicTacToe {
         currentPlayer = (currentPlayer == player1) ? player2 : player1;
     }
 
-    //TODO: Game Results public boolean hasWinner() {}
+    public boolean hasWinner() {
+        char[][] cells = board.getCells();
+        char marker = currentPlayer.getMarker();
+
+        // Check rows and columns
+        for (int i = 0; i < 3; i++) {
+            if ((cells[i][0] == marker && cells[i][1] == marker && cells[i][2] == marker) ||
+                    (cells[0][i] == marker && cells[1][i] == marker && cells[2][i] == marker)) {
+                return true;
+            }
+        }
+
+        // Check diagonals
+        if ((cells[0][0] == marker && cells[1][1] == marker && cells[2][2] == marker) ||
+                (cells[0][2] == marker && cells[1][1] == marker && cells[2][0] == marker)) {
+            return true;
+        }
+
+        return false;
+    }
 }
